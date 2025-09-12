1aa8854b9d48282489b04febfd7c34adde337077
Luciano Lorenzo <LuchoLorenzo1@users.noreply.github.com>
2025-09-12 09:46:22 -0300

    making the scope check the previous scope if a variable is not define in the current one

--- src/executor.rs
+++ src/executor.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
 use crate::{
     parser::{Primary, Statement},
@@ -7,36 +7,64 @@ use crate::{
 };
 
 #[derive(Debug)]
-pub struct Scope {
-    variables: HashMap<String, Primary>,
+pub struct Scope<'a> {
+    variables: RefCell<HashMap<String, Primary>>,
+    prev_scope: Option<&'a Scope<'a>>,
 }
 
-impl Scope {
+impl<'a> Scope<'a> {
     pub fn new() -> Self {
         Scope {
-            variables: HashMap::new(),
+            variables: RefCell::new(HashMap::new()),
+            prev_scope: None,
         }
     }
 
-    fn set(&mut self, var: String, exec: Primary) {
-        self.variables.insert(var, exec);
+    fn set(&self, var: String, exec: Primary) {
+        let mut vars = self.variables.borrow_mut();
+        vars.insert(var, exec);
     }
 
-    pub(crate) fn get(&self, v: &String) -> Primary {
-        if let Some(p) = self.variables.get(v) {
+    pub fn get(&self, v: &String) -> Primary {
+        let vars = self.variables.borrow();
+        if let Some(p) = vars.get(v) {
             p.clone()
         } else {
-            Primary::Null
+            if let Some(_s) = self.prev_scope {
+                _s.get(v)
+            } else {
+                Primary::Null
+            }
+        }
+    }
+
+    fn from_scope(scope: &'a Scope) -> Self {
+        Scope {
+            variables: scope.variables.clone(),
+            prev_scope: Some(scope),
         }
     }
 }
 
-pub fn execute_statement(statement: Statement, scope: &mut Scope) -> Result<Primary, ParserError> {
+pub fn execute_statement(statement: Statement, scope: &Scope) -> Result<Primary, ParserError> {
     match statement {
         Statement::Expression(expr) => expr.exec(&scope),
         Statement::Let(var, expr) => {
             let value = expr.exec(&scope)?;
             scope.set(var, value.clone());
+
+            Ok(Primary::Null)
+        }
+        Statement::If(conditional, stataments) => {
+            let value = conditional.exec(&scope)?;
+
+            let _scope = Scope::from_scope(scope);
+            if let Primary::True = value {
+                for i in stataments {
+                    execute_statement(i, &_scope)?;
+                }
+            }
+
             Ok(Primary::Null)
         }
         _ => perr!(syntax "Unsupported statement")?,
